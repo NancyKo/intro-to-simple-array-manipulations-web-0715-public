06874442a5a5262fc1434bfde4d041c518453fb6
NancyKo <konancys@gmail.com>
2015-07-30 22:37:25 -0400

    All finished

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,35 @@ end
 def pop_with_args(array)
   updated_array = array.pop(2)
 end
+
+def using_shift(array)
+  fist_element = array.shift
+end
+
+def shift_with_args(array)
+  first_two = array.shift(2)
+end
+
+def using_concat(array, second_array)
+  array.concat(second_array)
+end
+
+def using_insert(array, new_element)
+  array.insert(4, new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, some_string)
+  array.delete(some_string)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
+end
